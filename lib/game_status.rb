2b3db331a575e32f2403e9e15acd8e3344ef87cd
Iris Quintero <godessiris@gmail.com>
2019-05-19 19:50:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,34 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6],[3,4,5],[6,7,8]]
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def won?(board)
+if 
+  board[" "]
+return false 
+end
+
+ 
+
+  
+  
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  win_index_4 = win_combination[3]
+  win_index_5 = win_combination[4]
+  win_index_6 = win_combination[5]
+  win_index_7 = win_combination[6]
+  win_index_8 = win_combination[7]
+  
+
+  
+WIN_COMBINATIONS.each do |win_rows|
+  puts win_rows
+end
+  
+  
+
